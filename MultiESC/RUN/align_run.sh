a0a2ac7cf36b991ec5d9ba882c6c0b56af6bd5a4
wangjs9 <398128267@qq.com>
2023-11-06 15:19:02 +0800

    --upload_models

--- MultiESC/RUN/align_run.sh
+++ MultiESC/RUN/align_run.sh
@@ -9,7 +9,7 @@ nohup python -m align_MultiESC --data_type=8 \
   --model_type=1 \
   --candidate_num=10 \
   --preference_model_dir=../preference_modeling/output/esc_d-PM_23-0527-2227_fold1 \
-  --per_device_train_batch_size=7 \
+  --per_device_train_batch_size=8 \
   --seed=1 \
   --output_dir=./aligned_output/align_sample_10_d-PM > update_MultiESC.log 2>&1 &
 
